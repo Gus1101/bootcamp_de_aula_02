2f6cd9bd29aee59c91c5e1abd0bcfe49411702a9
Gus1101 <augustosantosb60@gmail.com>
2024-04-15 16:23:40 -0300

    Inserção da feature para calculo da área de um circulo

---
+++ example.py
@@ -0,0 +1,8 @@
+#Calculando a area de um circulo com a library math
+
+import math
+
+raio_do_circulo = int(input("Digite o raio do circulo: "))
+area_do_circulo = 2 * math.pi * raio_do_circulo
+
+print (area_do_circulo)
