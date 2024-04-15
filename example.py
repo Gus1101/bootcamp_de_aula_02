ec2946040de2acad21476329acc7efebb657f4b1
Gus1101 <augustosantosb60@gmail.com>
2024-04-15 16:25:12 -0300

    Correação na linha 6 para formula correta

--- example.py
+++ example.py
@@ -3,6 +3,6 @@
 import math
 
 raio_do_circulo = int(input("Digite o raio do circulo: "))
-area_do_circulo = 2 * math.pi * raio_do_circulo
+area_do_circulo = math.pi * raio_do_circulo ^ 2
 
 print (area_do_circulo)
